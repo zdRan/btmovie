39ba3e74757fa3d0b8d6cda5dcf356ade2546ba4
zdRan <cm.zdran@gmail.com>
2018-07-30 23:43:15 +0800

    添加 电影详情的获取

--- btmovie-crawler/crawler_application.py
+++ btmovie-crawler/crawler_application.py
@@ -21,7 +21,9 @@ def get_info(info_sub_url):
         "User-Agent":
             "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36",
     }
-    info_resp = movie_info = requests.get("http://www.btbtdy.net" + info_sub_url, headers=header)
+    movie_info = requests.get("http://www.btbtdy.net" + info_sub_url, headers=header)
+
+    return BeautifulSoup(movie_info.text, "html.parser")
 
 if __name__ == "__main__":
     soup = get_page("1")
@@ -30,15 +32,31 @@ if __name__ == "__main__":
     movie_info_arr = []
 
     for item in movie_arr:
-        print(item)
-        # movie_info_item = {
-        #     "movie_id": re.split("\.|/", item["href"])[2],
-        #     "movie_name": item["title"],
-        #     "movie_poster": item.contents[0].get("data-src"),
-        # }
+        movie_info_item = {}
+
+        # 获取评分
+        if item.get("class") is None:
+            movie_info_item["movie_score"] = item.next_sibling.string
+        else:
+            movie_info_item["movie_id"] = re.split("\.|/", item["href"])[2]
+            movie_info_item["movie_name"] = item["title"]
+            movie_info_item["movie_poster"] = item.contents[0].get("data-src")
+            item_info = get_info(item["href"])
+            info = item_info.find("div",class_="c05")
+            movie_info_item["movie_detail"] = info
+            movie_type = item_info.find("a",href=re.compile("^/screen/0-%.*$"))
+            print(item_info.text)
+            print(movie_type.text)
+            bt_html = get_info("/downlist/"+movie_info_item["movie_id"][2:]+"-0-0.html")
+            bt_arr = bt_html.find("a",href=re.compile("^thunder.*$"))
+            movie_info_item["movie_bt"] = bt_arr.get("href")
+
+        movie_info_arr.append(movie_info_item)
 
 
-    #get_info("/btdy/dy13279.html")
-    #get_info("/downlist/13279-0-0.html")
+    print(movie_info_arr)
+
+#get_info("/btdy/dy13279.html")
+    #get_info("13279-0-0.html")
 
     db = service.connect("localhost", 3306, "root", "root", "BT_MOVIE")
