dc97a578bde63241b12b63e6351ac58c78140448
zdRan <cm.zdran@gmail.com>
2018-08-01 23:15:58 +0800

    获取电影类型

--- btmovie-crawler/crawler_application.py
+++ btmovie-crawler/crawler_application.py
@@ -12,7 +12,6 @@ def get_page(index):
             "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36",
     }
     page_resp = requests.get("http://www.btbtdy.net/btfl/dy" + index + ".html", headers=header)
-
     return BeautifulSoup(page_resp.text, "html.parser")
 
 
@@ -22,41 +21,62 @@ def get_info(info_sub_url):
             "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36",
     }
     movie_info = requests.get("http://www.btbtdy.net" + info_sub_url, headers=header)
-
     return BeautifulSoup(movie_info.text, "html.parser")
 
-if __name__ == "__main__":
-    soup = get_page("1")
-
-    movie_arr = soup.find_all("a", href=re.compile(".*/btdy/.*"))
+def gen_info(movie_arr,movie_type_arr,movie_poster_arr):
     movie_info_arr = []
+    movie_index = 0
+
+    movie_score_arr = []
 
     for item in movie_arr:
+        if item.get("class") is None:
+            movie_score_arr.append(item.next_sibling.string)
+            movie_arr.remove(item)
+    for item in movie_arr:
+
         movie_info_item = {}
 
-        # 获取评分
-        if item.get("class") is None:
-            movie_info_item["movie_score"] = item.next_sibling.string
-        else:
-            movie_info_item["movie_id"] = re.split("\.|/", item["href"])[2]
-            movie_info_item["movie_name"] = item["title"]
-            movie_info_item["movie_poster"] = item.contents[0].get("data-src")
-            item_info = get_info(item["href"])
-            info = item_info.find("div",class_="c05")
-            movie_info_item["movie_detail"] = info
-            movie_type = item_info.find("a",href=re.compile("^/screen/0-%.*$"))
-            print(item_info.text)
-            print(movie_type.text)
-            bt_html = get_info("/downlist/"+movie_info_item["movie_id"][2:]+"-0-0.html")
-            bt_arr = bt_html.find("a",href=re.compile("^thunder.*$"))
-            movie_info_item["movie_bt"] = bt_arr.get("href")
+        movie_info_item["movie_id"] = re.split("\.|/", item["href"])[2]
+        movie_info_item["movie_name"] = item["title"]
+
+        item_info = get_info(item["href"])
+        info = item_info.find("div",class_="c05")
+        movie_info_item["movie_detail"] = info.text
+        # 获取电影类型
+        movie_info_item["movie_type"] = \
+            ",".join(re.split(" |,",movie_type_arr[movie_index].string))
+        movie_info_item["movie_score"] = movie_score_arr[movie_index]
+        movie_info_item["movie_poster"] = movie_poster_arr[movie_index].get("data-src")
+
+        movie_index += 1
+        # 获取主演
+        starring_tag_arr = item_info.find_all("a",href = re.compile("^/search/.*$"))
+        starring_arr = []
+        for starring_item in starring_tag_arr:
+            starring_arr.append(starring_item.text)
+        movie_info_item["movie_starring"] = ",".join(starring_arr)
+        # 获取下载地址
+        bt_html = get_info("/vidlist/"+movie_info_item["movie_id"][2:]+".html")
+
+        bt_arr = bt_html.find("a",href=re.compile("^magnet.*$"),class_="d1")
+        movie_info_item["movie_bt"] = bt_arr.get("href")
 
         movie_info_arr.append(movie_info_item)
 
+    return movie_info_arr
+if __name__ == "__main__":
+    for i in range(292):
+        page = get_page(str(i))
+        result = gen_info(page.find_all("a", href=re.compile(".*/btdy/.*")),
+                          page.find_all("p",class_ = "des"),
+                          page.find_all("img",attrs = {"data-src":re.compile("http://gif-china.cc/")},class_ = "lazy"))
+        item_index = 1
+        for info_item in result:
+            service.insert_movie_info(info_item,"localhost", 3306, "root", "root", "BT_MOVIE")
+            service.insert_movie_type(info_item,"localhost", 3306, "root", "root", "BT_MOVIE")
+            print("第 {0} 页,第 {1} 条数据".format(i,item_index))
+            item_index+=1
 
-    print(movie_info_arr)
 
-#get_info("/btdy/dy13279.html")
-    #get_info("13279-0-0.html")
 
-    db = service.connect("localhost", 3306, "root", "root", "BT_MOVIE")
--- btmovie-crawler/service/btmovie_service.py
+++ btmovie-crawler/service/btmovie_service.py
@@ -1,6 +1,6 @@
 import pymysql.cursors
 
-def connect(host, port,username, password, database):
+def insert_movie_info(info,host, port,username, password, database):
     db = pymysql.connect(host=host,
                          port=port,
                          user=username,
@@ -8,23 +8,50 @@ def connect(host, port,username, password, database):
                          db=database,
                          charset='utf8mb4',
                          cursorclass=pymysql.cursors.DictCursor)
-    return db
+    try:
+        with db.cursor() as cursor:
+            sql = "INSERT INTO movie_info VALUES (\'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\')"\
+                .format(info["movie_id"],info["movie_name"],info["movie_bt"],info["movie_poster"],
+                        info["movie_detail"],str(info["movie_score"]),info["movie_type"],
+                        info["movie_starring"],info["movie_type"].split(",")[1])
+            cursor.execute(sql)
+            db.commit()
+    finally:
+        db.close()
 
-def insert_movie_info(db,movie_info):
+
+def insert_movie_type(info,host, port,username, password, database):
+    db = pymysql.connect(host=host,
+                         port=port,
+                         user=username,
+                         password=password,
+                         db=database,
+                         charset='utf8mb4',
+                         cursorclass=pymysql.cursors.DictCursor)
+    try:
+        for item in  info["movie_type"].split(","):
+            count = select_movie_type(item,host, port,username, password, database)
+            if count == 0:
+                cursor = db.cursor()
+                sql = "INSERT INTO movie_type(type_name) VALUES (\'{}\')".format(item)
+                cursor.execute(sql)
+                db.commit()
+    finally:
+        db.close()
+
+def select_movie_type(item,host, port,username, password, database):
+    db = pymysql.connect(host=host,
+                         port=port,
+                         user=username,
+                         password=password,
+                         db=database,
+                         charset='utf8mb4',
+                         cursorclass=pymysql.cursors.DictCursor)
 
     try:
         with db.cursor() as cursor:
-            sql = "INSERT INTO 'movie_info' (" \
-                  "'movie_id'," \
-                  "'movie_name'," \
-                  "'movie_bt'," \
-                  "'movie_poster'," \
-                  "'movie_detail'," \
-                  "'movie_score'," \
-                  "'movie_type'," \
-                  "'movie_Starring'," \
-                  "'movie_area') VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-            cursor.execute(sql, (moiv_info["id"],moiv_info[""]))
-        db.commit()
+            sql = "SELECT type_id FROM movie_type WHERE type_name = \'{}\'".format(item)
+            cursor.execute(sql)
+            return len(cursor.fetchall())
     finally:
         db.close()
