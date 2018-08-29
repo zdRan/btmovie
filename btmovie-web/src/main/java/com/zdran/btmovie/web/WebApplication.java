cfd6869a968d67234b815af2c9a4913cffd28006
zdRan <cm.zdran@gmail.com>
2018-08-29 22:36:59 +0800

    添加程序入口

---
+++ btmovie-web/src/main/java/com/zdran/btmovie/web/WebApplication.java
@@ -0,0 +1,16 @@
+package com.zdran.btmovie.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zdRan on 2018/8/28
+ *
+ * @author cm.zdran@gmail.com
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
