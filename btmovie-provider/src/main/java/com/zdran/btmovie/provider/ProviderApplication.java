d98840279999ae7da162a88311eeb4d4edaa49ea
zdRan <cm.zdran@gmail.com>
2018-08-29 21:59:18 +0800

    添加PageHelper注入

--- btmovie-provider/src/main/java/com/zdran/btmovie/provider/ProviderApplication.java
+++ btmovie-provider/src/main/java/com/zdran/btmovie/provider/ProviderApplication.java
@@ -1,9 +1,13 @@
 package com.zdran.btmovie.provider;
 
+import com.github.pagehelper.PageHelper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Properties;
 
 /**
  * Created by zdRan on 2018/7/28
@@ -11,10 +15,22 @@ import org.springframework.context.annotation.ComponentScan;
  * @author cm.zdran@gmail.com
  */
 @SpringBootApplication
-@ComponentScan({"com.zdran.btmovie.provider","com.zdran.btmovie.common"})
+@ComponentScan({"com.zdran.btmovie.provider", "com.zdran.btmovie.common"})
 @MapperScan({"com.zdran.btmovie.provider.mapper"})
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @Bean
+    public PageHelper pageHelper() {
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+
+        properties.setProperty("offsetAsPageNum", "true");
+        properties.setProperty("rowBoundsWithCount", "true");
+        properties.setProperty("reasonable", "true");
+        pageHelper.setProperties(properties);
+        return pageHelper;
     }
 }
